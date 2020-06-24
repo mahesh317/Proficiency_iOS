d2348d04f50756c02c7ad1e36b66cb3a581869b6
Mahesh Miriyam <mahesh.miriyam@mindtree.com>
2020-06-24 22:42:18 +0530

    Initial commit

---
+++ proficiency_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proficiency_iOS
+//
+//  Created by Mahesh Miriyam on 24/06/20.
+//  Copyright © 2020 Mahesh Miriyam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ proficiency_iOSTests/proficiency_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  proficiency_iOSTests.swift
+//  proficiency_iOSTests
+//
+//  Created by Mahesh Miriyam on 24/06/20.
+//  Copyright © 2020 Mahesh Miriyam. All rights reserved.
+//
+
+import XCTest
+@testable import proficiency_iOS
+
+class proficiency_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
